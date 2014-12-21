f996065e8eb17f0272188544af95446df222627c
Asad Hasan <asad@dadesigners.com>
2014-12-21 13:07:05 -0600

    Catch redis exceptions.

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <?php
 header("Content-Type: application/json");
 if(!empty($_GET['ticket_id']) && !empty($_GET['register_id']) && !empty($_GET['op'])) {
-	$r = new Redis();
-	$r->connect("127.0.0.1", "6379");
+	try {
+		$r = new Redis();
+		$r->connect("127.0.0.1", "6379");
 
-	$ticket_id = "ticket:".$_GET['ticket_id'];
+		$ticket_id = "ticket:".$_GET['ticket_id'];
 
-	$user = $_GET['register_id'];
-	$op = $_GET['op'];
-	$key = "lock:" . $ticket_id;
+		$user = $_GET['register_id'];
+		$op = $_GET['op'];
+		$key = "lock:" . $ticket_id;
 
-	if($op == 'acquire' || $op == 'renew') {
-		$nx = $r->setnx($key, $user);
-		if ($nx || ($op == 'renew' && $r->get($key) == $user) || ($op == 'acquire' && $r->get($key) == $user)) {
-			// Because system can crash, give him 5 min.
-			$r->expire($key, 60 * 3);
-			echo json_encode(array("status" => true, "message" => "Ticket lock acquired."));
+		if($op == 'acquire' || $op == 'renew') {
+			$nx = $r->setnx($key, $user);
+			if ($nx || ($op == 'renew' && $r->get($key) == $user) || ($op == 'acquire' && $r->get($key) == $user)) {
+				// Because system can crash, give him 5 min.
+				$r->expire($key, 60 * 3);
+				echo json_encode(array("status" => true, "message" => "Ticket lock acquired."));
+			} else {
+				echo json_encode(array("status" => false, "message" => "Ticket is currently open at Register: ".$r->get($key)));
+			}
+		} elseif($op == 'unlock') {
+			if($r->get($key) == $user) {
+				$r->del($key);
+				echo json_encode(array("status" => true, "message" => "Successfully unlocked ticket."));
+			} else {
+				echo json_encode(array("status" => false, "message" => "Failed to unlock ticket."));
+			}
 		} else {
-			echo json_encode(array("status" => false, "message" => "Ticket is currently open at Register: ".$r->get($key)));
+			echo json_encode(array("status" => false, "message" => "Missing or invalid argument 'op'."));
 		}
-	} elseif($op == 'unlock') {
-		if($r->get($key) == $user) {
-			$r->del($key);
-			echo json_encode(array("status" => true, "message" => "Successfully unlocked ticket."));
-		} else {
-			echo json_encode(array("status" => false, "message" => "Failed to unlock ticket."));
-		}
-	} else {
-		echo json_encode(array("status" => false, "message" => "Missing or invalid argument 'op'."));
+	} catch (Exception $e) {
+		echo json_encode(array("status" => false, "message" => "Could not connect to lock server."));
 	}
 } else {
 	echo json_encode(array("status" => false, "message" => "Missing or invalid argument(s) 'op', 'ticket_id', or 'register_id'."));
